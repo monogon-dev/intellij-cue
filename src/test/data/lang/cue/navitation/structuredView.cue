1dbf765de7f8f9079625a17350cf1b06bd82c66c
Boris Smidt <boris.smidt@restore.eu>
2023-07-01 18:09:48 +0200

    Add a structured view test

---
+++ src/test/data/lang/cue/navitation/structuredView.cue
@@ -0,0 +1,21 @@
+package list
+
+Comparer: {
+	x:    T
+}
+
+ job: myTask: replicas: 2
+
+Alias = 3
+a: Alias
+
+h: {
+    let: "value"
+    number: let
+    float: number
+    string: float
+    bytes: string
+    uint: number
+    rune: uint
+    float64: rune
+}
--- src/test/java/dev/monogon/cue/lang/navigation/CueStructureViewElementTest.java
+++ src/test/java/dev/monogon/cue/lang/navigation/CueStructureViewElementTest.java
@@ -11,41 +11,56 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import com.intellij.util.Consumer;
 import dev.monogon.cue.CueLightTest;
 import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
 
-import static com.intellij.testFramework.PlatformTestUtil.assertTreeEqual;
-import static com.intellij.testFramework.PlatformTestUtil.expandAll;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.intellij.testFramework.PlatformTestUtil.*;
 
 /**
  * Copyright (C) 23.04.23 - REstore NV
  */
-//public class CueStructureViewElementTest extends CueLightTest {
-//    public void testShorthand() {
-//        doTest("-Shorthand.cue" +
-//                "   job.myTask.replicas");
-//    }
-//
-//    private void doTest(final String expected) {
-//        myFixture.configureByFile(getTestName(false) + ".cue");
-//        myFixture.testStructureView(svc -> {
-//            expandAll(svc.getTree());
-//            assertTreeEqual(svc.getTree(), expected);
-//        });
-//    }
-//
-//    private void doTestTreeStructure(@NotNull Consumer<StructureViewModel> consumer) {
-//        myFixture.configureByFile(getTestName(false) + ".cue");
-//        final StructureViewBuilder builder = LanguageStructureViewBuilder.INSTANCE.getStructureViewBuilder(myFixture.getFile());
-//        assertNotNull(builder);
-//        StructureViewComponent component = null;
-//        try {
-//            final FileEditor editor = FileEditorManager.getInstance(getProject()).getSelectedEditor(myFixture.getFile().getVirtualFile());
-//            component = (StructureViewComponent) builder.createStructureView(editor, myFixture.getProject());
-//            final StructureViewModel model = component.getTreeModel();
-//            consumer.consume(model);
-//        } finally {
-//            if (component != null) {
-//                Disposer.dispose(component);
-//            }
-//        }
-//    }
-//}
+public class CueStructureViewElementTest extends CueLightTest {
+    @Test
+    public void testStructuredView() {
+        doTest(false);
+    }
+
+    private void doTest(boolean overWriteResult) {
+        var expectedFile = Paths.get(myFixture.getTestDataPath() + "/lang/cue/navitation/" + getTestName(true) + ".txt");
+        myFixture.configureByFile("/lang/cue/navitation/" + getTestName(true) + ".cue");
+        myFixture.testStructureView(svc -> {
+            expandAll(svc.getTree());
+            try {
+                if (!overWriteResult) {
+                    String expected = Files.readString(expectedFile);
+                    assertTreeEqual(svc.getTree(), expected);
+                } else {
+                    Files.writeString(expectedFile, print(svc.getTree()));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public void doTestTreeStructure(@NotNull Consumer<StructureViewModel> consumer) {
+        myFixture.configureByFile(getTestName(false) + ".cue");
+        final StructureViewBuilder builder = LanguageStructureViewBuilder.INSTANCE.getStructureViewBuilder(myFixture.getFile());
+        assertNotNull(builder);
+        StructureViewComponent component = null;
+        try {
+            final FileEditor editor = FileEditorManager.getInstance(getProject()).getSelectedEditor(myFixture.getFile().getVirtualFile());
+            component = (StructureViewComponent) builder.createStructureView(editor, myFixture.getProject());
+            final StructureViewModel model = component.getTreeModel();
+            consumer.consume(model);
+        } finally {
+            if (component != null) {
+                Disposer.dispose(component);
+            }
+        }
+    }
+}
